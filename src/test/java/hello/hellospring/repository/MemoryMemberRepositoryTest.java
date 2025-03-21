267c8bed413c83fc85e7305f256afa2f93a9ec52
chahyeon min <chahyeonmin91@gmail.com>
2025-03-21 23:28:05 +0900

    test

--- src/test/java/hello/hellospring/repository/MemoryMemberRepositoryTest.java
+++ src/test/java/hello/hellospring/repository/MemoryMemberRepositoryTest.java
@@ -17,4 +17,16 @@ class MemoryMemberRepositoryTest {
         Member result = repository.findById(member.getId()).get();
         assertThat(member).isEqualTo(result);
     }
+
+    @Test
+    public void findByName() {
+        Member member1 = new Member();
+        member1.setName("spring1");
+        repository.save(member1);
+
+        Member member2 = new Member();
+        member2.setName("spring2");
+        repository.save(member2);
+
+    }
 }
